5163fa550dce0bc48eeaf4d486457c5ade712b2d
Evgeny Persienko <e.persienko@office.ngs.ru>
2016-06-16 15:40:45 +0600

    now dirty repos shown as not pushed

--- checkers.go
+++ checkers.go
@@ -1,13 +1,6 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"os"
-	"os/exec"
-	"regexp"
-	"strings"
-
 	"github.com/fatih/color"
 	"github.com/seletskiy/hierr"
 )
@@ -22,63 +15,6 @@ var (
 
 type checkFunc func(path, bool, bool) error
 
-func getCurrentBranch(dir string, quiet bool) (string, error) {
-	out, err := execute(dir, quiet, "branch", "--points-at", "HEAD")
-	if err != nil {
-		return "", err
-	}
-
-	branches := strings.Split(out, "\n")
-
-	for _, branch := range branches {
-		if branch[0] == '*' {
-			branch = strings.TrimLeft(branch, "* ")
-			branch = strings.TrimRight(branch, "\n")
-			return strings.TrimSpace(branch), nil
-		}
-	}
-
-	return "", errors.New("can't find current branch")
-}
-
-func isTracked(dir string, quiet bool) (bool, error) {
-	out, err := execute(dir, quiet, "status", "--porcelain", "-b")
-	if err != nil {
-		return false, err
-	}
-
-	branchInfo := strings.Split(out, "\n")[0]
-	trackedReg := regexp.MustCompile(`## [^.]+\.\.\.[^/]+/.+`)
-
-	return trackedReg.MatchString(branchInfo), nil
-}
-
-func isDetachedHead(dir string, quiet bool) (bool, error) {
-	branch, err := getCurrentBranch(dir, quiet)
-	if err != nil {
-		return false, err
-	}
-
-	return strings.Contains(branch, "detached"), nil
-}
-
-func stdCheck(path path, onlyDirty, quiet bool, commandLine ...string) error {
-	out, err := execute(path.linkTo, quiet, commandLine...)
-	if err != nil {
-		return err
-	}
-
-	if out != "" {
-		fmt.Printf("%s %s\n", dirtyMarker, path)
-		return nil
-	}
-
-	if !onlyDirty {
-		fmt.Printf("%s %s\n", cleanMarker, path)
-	}
-	return nil
-}
-
 func pushCheck(path path, onlyDirty, quiet bool) error {
 	detached, err := isDetachedHead(path.linkTo, quiet)
 	if err != nil {
@@ -86,10 +22,7 @@ func pushCheck(path path, onlyDirty, quiet bool) error {
 	}
 
 	if detached {
-		if !onlyDirty {
-			fmt.Printf("%s %s\n", cleanMarker, path)
-		}
-
+		printClean(path, onlyDirty)
 		return nil
 	}
 
@@ -99,15 +32,47 @@ func pushCheck(path path, onlyDirty, quiet bool) error {
 	}
 
 	if !tracked {
-		fmt.Printf("%s %s %s\n", dirtyMarker, path, red("(not tracked)"))
+		printDirty(path, "(not tracked)")
 		return nil
 	}
 
-	return stdCheck(path, onlyDirty, quiet, "log", "@{push}..")
+	dirty, err := isDirty(path.linkTo, quiet)
+	if err != nil {
+		return err
+	}
+
+	if dirty {
+		printDirty(path, "(dirty)")
+		return nil
+	}
+
+	pushed, err := isPushed(path.linkTo, quiet)
+	if err != nil {
+		return err
+	}
+
+	if pushed {
+		printClean(path, onlyDirty)
+	} else {
+		printDirty(path)
+	}
+
+	return nil
 }
 
 func dirtyCheck(path path, onlyDirty, quiet bool) error {
-	return stdCheck(path, onlyDirty, quiet, "status", "--porcelain")
+	dirty, err := isDirty(path.linkTo, quiet)
+	if err != nil {
+		return err
+	}
+
+	if dirty {
+		printDirty(path)
+	} else {
+		printClean(path, onlyDirty)
+	}
+
+	return nil
 }
 
 func showBranch(path path, onlyDirty, quiet bool) error {
@@ -117,29 +82,10 @@ func showBranch(path path, onlyDirty, quiet bool) error {
 	}
 
 	if branch == "master" {
-		branch = green(branch)
+		printClean(path, onlyDirty, branch)
 	} else {
-		branch = red(branch)
+		printDirty(path, branch)
 	}
 
-	fmt.Printf("%s %s\n", path, branch)
 	return nil
-}
-
-func execute(dir string, quiet bool, commandLine ...string) (string, error) {
-	cmd := exec.Command("git", commandLine...)
-	cmd.Dir = dir
-	if !quiet {
-		cmd.Stderr = os.Stderr
-	}
-
-	out, err := cmd.Output()
-	if err != nil {
-		return "", hierr.Errorf(
-			err, "can't run 'git %s' in '%s'",
-			strings.Join(commandLine, " "), dir,
-		)
-	}
-
-	return string(out), nil
 }
---
+++ utils.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+
+	"github.com/seletskiy/hierr"
+)
+
+func printDirty(path path, postfix ...string) {
+	if len(postfix) > 0 {
+		fmt.Printf("%s %s %s\n", dirtyMarker, path, red(postfix[0]))
+	} else {
+		fmt.Printf("%s %s\n", dirtyMarker, path)
+	}
+}
+
+func printClean(path path, onlyDirty bool, postfix ...string) {
+	if onlyDirty {
+		return
+	}
+
+	if len(postfix) > 0 {
+		fmt.Printf("%s %s %s\n", cleanMarker, path, green(postfix[0]))
+	} else {
+		fmt.Printf("%s %s\n", cleanMarker, path)
+	}
+}
+
+func getCurrentBranch(dir string, quiet bool) (string, error) {
+	out, err := execute(dir, quiet, "branch", "--points-at", "HEAD")
+	if err != nil {
+		return "", err
+	}
+
+	branches := strings.Split(out, "\n")
+
+	for _, branch := range branches {
+		if branch[0] == '*' {
+			branch = strings.TrimLeft(branch, "* ")
+			branch = strings.TrimRight(branch, "\n")
+			return strings.TrimSpace(branch), nil
+		}
+	}
+
+	return "", errors.New("can't find current branch")
+}
+
+func isTracked(dir string, quiet bool) (bool, error) {
+	out, err := execute(dir, quiet, "status", "--porcelain", "-b")
+	if err != nil {
+		return false, err
+	}
+
+	branchInfo := strings.Split(out, "\n")[0]
+	trackedReg := regexp.MustCompile(`## [^.]+\.\.\.[^/]+/.+`)
+
+	return trackedReg.MatchString(branchInfo), nil
+}
+
+func isDetachedHead(dir string, quiet bool) (bool, error) {
+	branch, err := getCurrentBranch(dir, quiet)
+	if err != nil {
+		return false, err
+	}
+
+	return strings.Contains(branch, "detached"), nil
+}
+
+func isDirty(dir string, quiet bool) (bool, error) {
+	out, err := execute(dir, quiet, "status", "--porcelain")
+	if err != nil {
+		return false, err
+	}
+
+	return out != "", nil
+}
+
+func isPushed(dir string, quiet bool) (bool, error) {
+	out, err := execute(dir, quiet, "log", "@{push}..")
+	if err != nil {
+		return false, err
+	}
+
+	return out == "", nil
+}
+
+func execute(dir string, quiet bool, commandLine ...string) (string, error) {
+	cmd := exec.Command("git", commandLine...)
+	cmd.Dir = dir
+	if !quiet {
+		cmd.Stderr = os.Stderr
+	}
+
+	out, err := cmd.Output()
+	if err != nil {
+		return "", hierr.Errorf(
+			err, "can't run 'git %s' in '%s'",
+			strings.Join(commandLine, " "), dir,
+		)
+	}
+
+	return string(out), nil
+}
